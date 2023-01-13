e182cc7fa24f654a6808662b73ae4d19de3121cc
Agrima <er.agrima.malhotra@gmail.com>
2023-01-13 15:49:46 +0530

    chore: add check for prime and armstrong

---
+++ task/prime-armstrong.js
@@ -0,0 +1,30 @@
+function  isprime(n){
+    for (let i=0; i< Math.sqrt(n);i+=2)
+    {
+        if (n%i === 0){
+            console.log('Not Prime.');
+            return;
+        }
+    }
+    console.log('Is prime.');
+    return;
+}
+function isarmsrtong(n){
+    const num=n;
+    let sum=0;
+    while (n>0){
+        let r=n%10;
+        sum+=r*r*r;
+        n=parseInt(n/10);
+    }
+    if (sum === num){
+        console.log('Is Armstrong');
+    }
+    else {
+        console.log('Not Armstrong');
+    }
+    return;
+}
+
+isarmsrtong(153);
+isprime(17);
